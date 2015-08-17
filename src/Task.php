2a3f669e3cc5465f05dddded1ccc8cc118db9b17
Ian McKenney & Casey Heitz <student@epicodus.com>
2015-08-17 11:57:04 -0700

    Change tests for getAll, deleteAll, and save for MySQL

--- src/Task.php
+++ src/Task.php
@@ -1,34 +1,47 @@
 <?php
+
 class Task
-{
-    private $description;
+  {
+      private $description;
+    //  private $id;
 
-    function __construct($description)
-    {
-        $this->description = $description;
-    }
+      function __construct($description)
+      {
+          $this->description = $description;
+    //      $this->id = $id;
+      }
 
-    function setDescription($new_description)
-    {
-        $this->description = (string) $new_description;
-    }
+      function setDescription($new_description)
+      {
+          $this->description = (string) $new_description;
+      }
 
-    function getDescription()
-    {
-        return $this->description;
-    }
+      function getDescription()
+      {
+          return $this->description;
+      }
 
-    function save()
-    {
-        array_push($_SESSION['list_of_tasks'], $this);
-    }
-    static function getAll()
-    {
-        return $_SESSION['list_of_tasks'];
-    }
-    static function deleteAll()
-    {
-        $_SESSION['list_of_tasks'] = array();
-    }
-}
+      function save()
+      {
+          $GLOBALS['DB']->exec("INSERT INTO tasks (description) VALUES ('{$this->getDescription()}');");
+      }
+
+      static function getAll()
+      {
+          $returned_tasks = $GLOBALS['DB']->query("SELECT * FROM tasks;");
+          $tasks = array();
+          foreach($returned_tasks as $task) {
+            $description = $task['description'];
+            $id = $task['id'];
+            $new_task = new Task($description, $id);
+            array_push($tasks, $new_task);
+          }
+          return $tasks;
+      }
+
+      static function deleteAll()
+      {
+          $GLOBALS['DB']->exec("DELETE FROM tasks;");
+      }
+  }
 ?>
---
+++ tests/TaskTest.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+* @backupGlobals disabled
+* @backupStaticAttributes disabled
+*/
+
+  require_once "src/Task.php";
+
+  $server = 'mysql:host=localhost;dbname=to_do_test';
+  $username = 'root';
+  $password = 'root';
+  $DB = new PDO($server, $username, $password);
+
+  class TaskTest extends PHPUnit_Framework_TestCase
+  {
+      protected function tearDown()
+      {
+        Task::deleteAll();
+      }
+
+      function test_save()
+      {
+        $description = "Wash the dog";
+        $test_task = new Task($description);
+
+        $test_task->save();
+
+        $result = Task::getAll();
+        $this->assertEquals($test_task, $result[0]);
+      }
+
+      function test_getAll()
+      {
+        $description = "Wash the dog";
+        $description2 = "Water the lawn";
+        $test_task = new Task($description);
+        $test_task->save();
+        $test_task2 = new Task($description2);
+        $test_task2->save();
+
+        $result = Task::getAll();
+
+        $this->assertEquals([$test_task, $test_task2], $result);
+      }
+
+      function test_deleteAll()
+      {
+        $description = "Wash the dog";
+        $description2 = "Water the lawn";
+        $test_task = new Task($description);
+        $test_task->save();
+        $test_task2 = new Task($description2);
+        $test_task2->save();
+
+        Task::deleteAll();
+
+        $result = Task::getAll();
+        $this->assertEquals([],$result);
+      }
+  }
+?>
