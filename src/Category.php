ca312e412a25ec75cbcdf16e748d3b7028b73564
Ian McKenney & Casey Heitz <student@epicodus.com>
2015-08-17 12:27:47 -0700

    write Category Class and tests

---
+++ src/Category.php
@@ -0,0 +1,73 @@
+<?php
+
+class Category
+  {
+      private $name;
+      private $id;
+
+      function __construct($name, $id=null)
+      {
+          $this->name = $name;
+          $this->id = $id;
+      }
+
+      function getId()
+      {
+          return $this->id;
+      }
+
+      function setId($new_id)
+      {
+        $this->id = $new_id;
+      }
+
+      function setName($new_name)
+      {
+          $this->name = (string) $new_name;
+      }
+
+      function getName()
+      {
+          return $this->name;
+      }
+
+      function save()
+      {
+          $GLOBALS['DB']->exec("INSERT INTO categories (name) VALUES ('{$this->getName()}');");
+          //$this->id = $GLOBALS['DB']->lastInsertId();
+          $result_id = $GLOBALS['DB']->lastInsertId();
+          $this->setId($result_id);
+      }
+
+      static function getAll()
+      {
+          $returned_categories = $GLOBALS['DB']->query("SELECT * FROM categories;");
+          $categories = array();
+          foreach($returned_categories as $category) {
+            $name = $category['name'];
+            $id = $category['id'];
+            $new_categories = new Category($name, $id);
+            array_push($categories, $new_category);
+          }
+          return $categories;
+      }
+
+      static function deleteAll()
+      {
+          $GLOBALS['DB']->exec("DELETE FROM categories;");
+      }
+
+      static function find($search_id)
+      {
+          $found_category = NULL;
+          $categories = Category::getAll();
+          foreach($categories as $category) {
+            $category_id = $category->getId();
+            if ($category_id == $search_id) {
+              $found_category = $category;
+            }
+          }
+          return $found_category;
+      }
+  }
+?>
---
+++ tests/CategoryTest.php
@@ -0,0 +1,86 @@
+<?php
+
+  /**
+  * @backupGlobals disabled
+  * @backupStaticAttributes disabled
+  */
+
+  require_once "src/Category.php";
+
+  $server = 'mysql:host=localhost;dbname=to_do_test';
+  $username = 'root';
+  $password = 'root';
+  $DB = new PDO($server, $username, $password);
+
+  class CategoryTest extends PHPUnit_Framework_TestCase
+  {
+    protected function tearDown()
+    {
+      Category::deleteAll();
+    }
+
+    function test_getName()
+    {
+      $name = "Work stuff";
+      $test_Category = new Category($name);
+
+      $result = $test_Category->getName();
+
+      $this->assertEquals($name, $result);
+    }
+
+    function test_getId()
+    {
+      $name = "Work stuff";
+      $id = 1;
+      $test_Category = new Category($name, $id);
+
+      $result = $test_Category->getId();
+
+      $this->assertEquals(true, is_numeric($result));
+    }
+
+    function test_save()
+    {
+      $name = "Work stuff";
+      $test_Category = new Category($name);
+      $test_Category->save();
+
+      $result = Category::getAll();
+
+      $this->assertEquals($test_Category, $result[0]);
+
+    }
+
+    function test_getAll()
+    {
+      $name = "Work stuff";
+      $name2 = "Home stuff";
+      $test_Category = new Category($name);
+      $test_Category->save();
+      $test_Category2 = new Category($name2);
+      $test_Category2->save();
+
+      $result = Category::getAll();
+
+      $this->assertEquals([$test_Category,$test_Category2], $result);
+    }
+
+    function test_deleteAll()
+    {
+      $name = "Wash the dog";
+      $name2 = "Home stuff";
+      $test_Category = new Category($name);
+      $test_Category->save();
+      $test_Category2 = new Category($name2);
+      $test_Category2->save();
+
+      Category::deleteAll();
+      $result = Category::getAll();
+
+      $this->assertEquals([], $result);
+    }
+
+  }
+
+?>
