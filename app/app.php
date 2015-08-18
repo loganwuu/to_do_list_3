a2ffff3ad173225ce430377ad5bfbc20221b241b
logan wu & ian mckenney <student@epicodus.com>
2015-08-18 12:26:27 -0700

    add due-date form to category.html.twig

--- app/app.php
+++ app/app.php
@@ -31,16 +31,16 @@
     $app->post("/tasks", function() use ($app) {
         $description = $_POST['description'];
         $category_id = $_POST['category_id'];
-        $task = new Task($description, $id = null, $category_id);
+        $due_date = $_POST['due_date'];
+        $task = new Task($description, $id = null, $category_id, $due_date);
         $task->save();
         $category = Category::find($category_id);
         return $app['twig']->render('category.html.twig', array('category' => $category, 'tasks' => Task::getAll()));
     });
 
-   $app->post("/delete_tasks/categories/{id}", function($id)  use ($app){
-        $category = Category::find($id);
+   $app->post("/delete_tasks", function()  use ($app){
         Task::deleteAll();
-        return $app['twig']->render('category.html.twig', array('category' => $category, 'tasks' => Task::getAll()));
+        return $app['twig']->render('index.html.twig', array('categories' => Category::getAll()));
     });
 
     $app->post("/categories", function() use ($app) {
