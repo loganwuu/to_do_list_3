bfaa899d1dab13dbc32c21532caad06a9559bc4d
logan wu & ian mckenney <student@epicodus.com>
2015-08-18 10:10:59 -0700

    Change Task.php to have category_Id

--- src/Task.php
+++ src/Task.php
@@ -3,17 +3,24 @@
 class Task
   {
       private $description;
+      private $category_id;
       private $id;
 
-      function __construct($description, $id=null)
+      function __construct($description, $id=null, $category_id)
       {
           $this->description = $description;
           $this->id = $id;
+          $this->category_id = $category_id;
       }
 
       function getId()
       {
           return $this->id;
+      }
+
+      function getCategoryId()
+      {
+          return $this->category_id;
       }
 
       function setDescription($new_description)
@@ -28,7 +35,7 @@ class Task
 
       function save()
       {
-          $GLOBALS['DB']->exec("INSERT INTO tasks (description) VALUES ('{$this->getDescription()}');");
+          $GLOBALS['DB']->exec("INSERT INTO tasks (description, category_id) VALUES ('{$this->getDescription()}', {$this->getCategoryId()});");
           $this->id = $GLOBALS['DB']->lastInsertId();
       }
 
@@ -39,7 +46,8 @@ class Task
           foreach($returned_tasks as $task) {
             $description = $task['description'];
             $id = $task['id'];
-            $new_task = new Task($description, $id);
+            $category_id = $task['category_id'];
+            $new_task = new Task($description, $id, $category_id);
             array_push($tasks, $new_task);
           }
           return $tasks;
