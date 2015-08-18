7aec6163ec91890441c2a638c964204fe453ac49
logan wu & ian mckenney <student@epicodus.com>
2015-08-18 11:11:52 -0700

    Add clear categories button

--- app/app.php
+++ app/app.php
@@ -19,25 +19,29 @@
       return $app['twig']->render('index.html.twig', array('categories' => Category::getAll()));
     });
 
-    $app->get("/tasks", function() use ($app) {
+/*    $app->get("/tasks", function() use ($app) {
         return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
-    });
+    });*/
 
     $app->get("/categories/{id}", function($id) use ($app) {
-        $category = Category::find();
-        return $app['twig']->render('category.html.twig', array('categories' => $category, 'tasks' => $category->getTasks()));
+        $category = Category::find($id);
+        return $app['twig']->render('category.html.twig', array('category' => $category, 'tasks' => $category->getTasks()));
     });
 
     $app->post("/tasks", function() use ($app) {
-        $task = new Task($_POST['description']);
+        $description = $_POST['description'];
+        $category_id = $_POST['category_id'];
+        $task = new Task($description, $id = null, $category_id);
         $task->save();
-        return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll(), 'newtask' => $task));
+        $category = Category::find($category_id);
+        return $app['twig']->render('category.html.twig', array('category' => $category, 'tasks' => Task::getAll()));
     });
 
-    $app->post("/delete_tasks", function()  use ($app){
-            Task::deleteAll();
-            return $app['twig']->render('index.html.twig');
-    });
+/*    $app->post("/delete_tasks/categories/{id}", function($id)  use ($app){
+        $category = Category::find($id);
+        Task::deleteAll();
+        return $app['twig']->render('category.html.twig', array('category' => $category, 'tasks' => Task::getAll()));
+    });*/
 
     $app->post("/categories", function () use ($app) {
       $category = new Category($_POST['name']);
@@ -47,7 +51,7 @@
 
     $app->post("/delete_categories", function() use($app) {
       Category::deleteAll();
-      return $app['twig']->render('index.html.twig');
+      return $app['twig']->render('index.html.twig', array('categories' => Category::getall()));
     });
 
     return $app;
